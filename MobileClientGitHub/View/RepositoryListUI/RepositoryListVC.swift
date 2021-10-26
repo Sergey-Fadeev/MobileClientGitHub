04b603d80e8c74f90a335ffac0ecf6d3b62279e7
Sergey Fadeev <59198513+Sergey-Fadeev@users.noreply.github.com>
2021-10-26 12:10:10 +0400

    Initial Commit

---
+++ MobileClientGitHub/View/RepositoryListUI/RepositoryListVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileClientGitHub
+//
+//  Created by Sergey on 26.10.2021.
+//
+
+import UIKit
+
+class RepositoryListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MobileClientGitHub/View/RepositoryListUI/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  MobileClientGitHub
+//
+//  Created by Sergey on 26.10.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
