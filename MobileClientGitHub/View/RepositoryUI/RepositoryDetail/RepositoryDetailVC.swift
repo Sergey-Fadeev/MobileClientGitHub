d6a42ae242a07dad5e83db90a9fe894465224657
Sergey Fadeev <59198513+Sergey-Fadeev@users.noreply.github.com>
2021-11-12 14:28:15 +0400

    interfaces and classes for detail

---
+++ MobileClientGitHub/View/RepositoryUI/RepositoryDetail/RepositoryDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  RepositoryDetailVC.swift
+//  MobileClientGitHub
+//
+//  Created by Sergey on 12.11.2021.
+//
+
+import UIKit
+
+class RepositoryDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
